3e656d6ee031833fe2bd679562e169501ae07f9d
W0085438 <90987029+W0085438@users.noreply.github.com>
2021-12-01 04:00:20 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,33 +3,33 @@ $(document).ready(function() {
 	
 	$.getJSON("matrix.json", function (data){
 		
-		console.log(data);
+		
 		
 			var character = ''; 
             $.each(data, function (key, value) {
 				
 				character += '<tr>';
-                character += '<td id="name">' + 
+                character += '<td class="name">' + 
 				value.name + '</td>';
   
-                character += '<td id="bd">' + 
+                character += '<td class="bd">' + 
                 value.birthdate + '</td>';
   
-                character += '<td id="pill">' + 
+                character += '<td class="pill">' + 
                 value.pill + '</td>';
   
-                character += '<td id="sex">' + 
+                character += '<td class="sex">' + 
                 value.sex + '</td>';
   
                 character += '</tr>';
                });
                           
-                $('#charlist').append(character);              
-            });
+                $('#charlist').append(character);    
+		});	
 	$('#nSort').on('click', function() {
-		var nameText = $('#name');
-		var names = $('#charlist').find(nameText).text();
-		console.log(names);
-	});		
-			
+			var nameText = [];
+			var names = nameText.push($('.name').text());
+			console.log(names);
+	
+		});	
 });
