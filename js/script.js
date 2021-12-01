b432c281693a9c9db5e4068525ffaa11fc05b487
W0085438 <90987029+W0085438@users.noreply.github.com>
2021-12-01 03:05:19 -0500

    creating event listener for name sorting

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
 
 $(document).ready(function() {
 	
-	$.getJSON("matrix.json", function(data){
-            console.log(data.name); 
-            console.log(data.birthdate);
-			console.log(data.pill);
-			console.log(data.sex);
-        }).fail(function(){
-            console.log("An error has occurred while loading JSON data.");
-        });
+	$.getJSON("matrix.json", function (data){
+		
+		console.log(data);
+		
+			var character = ''; 
+            $.each(data, function (key, value) {
+				
+				character += '<tr>';
+                character += '<td id="name">' + 
+				value.name + '</td>';
+  
+                character += '<td id="bd">' + 
+                value.birthdate + '</td>';
+  
+                character += '<td id="pill">' + 
+                value.pill + '</td>';
+  
+                character += '<td id="sex">' + 
+                value.sex + '</td>';
+  
+                character += '</tr>';
+               });
+                          
+                $('#charlist').append(character);              
+            });
+	$('#nSort').on('click', function() {
+		var nameText = $('#name');
+		var names = $('#charlist').find(nameText).text();
+		console.log(names);
+	});		
+			
 });
